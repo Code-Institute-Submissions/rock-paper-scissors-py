6abf166957885d13a9a3e16e8f21fe19c2c24fa5
Jean-Pierre Touma <jp.touma@outlook.com>
2021-11-25 11:05:45 +0000

    Add pick_option function to get player input then check its validity and return the valid value

--- run.py
+++ run.py
@@ -1,10 +1,25 @@
 from random import randint
 
-# Stores a list of game options
-game_options = ["Rock", "Paper", "Scissors"]
+# Stores wins for the player and computer
+player_wins = 0
+cpu_wins = 0
 
-# Assign a random option of the game_options list
-cpu = game_options[randint(0,2)]
 
-# Checks if the player is playing
-player = False
+def pick_option():
+    """
+    Provides input for the player to enter their play option and then checks if
+    the entered input key is valid and then return the choice of the player
+    """
+    player_choice = input("Choose Rock, Paper, or Scissors: ")
+    if player_choice in ["Rock", "rock", "r", "R"]:
+        player_choice = "r"
+    elif player_choice in ["Paper", "paper", "p", "P"]:
+        player_choice = "p"
+    elif player_choice in ["Scissors", "scissors", "s", "S"]:
+        player_choice = "p"
+    else:
+        print("Invalid input, try using r = Rock, p = Paper, s = Scissors")
+        pick_option()
+    return player_choice
+
+pick_option()
